8fdde1c201af14ddad948565403bc6d17ad3f325
Teddie <ibonisaledu@gmail.com>
2024-01-01 11:38:54 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,41 @@
+// FOR MENU BAR
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function () {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+// FOR TAB SWITCHING
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab")
+}
+// FOR CONTACT FORM
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwMv01nhgVR6SJvONfTncnw4wpJtac_4Z7ue1ayw71eiLLx37ae5E6yy-14o5qMAfr2IQ/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message Submitted ✌🤙"
+            setTimeout(function () {
+                msg.innerHTML = ""
+            }, 5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
